9b672d3b991b2ce8765a1a8b3a30a3db624c7bb8
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2023-08-08 16:07:44 +0530

    Add files via upload

---
+++ Random_Password_Generator_With_Special_Character.py
@@ -0,0 +1,8 @@
+from random import *
+alphabet="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+digit="0123456789"
+special="!@#$%^&*()-+=;:'\/`~,<>?"
+
+print(choice(alphabet + digit + special),choice(alphabet + digit + special),choice(alphabet + digit + special),choice(alphabet + digit + special),choice(alphabet + digit + special),choice(alphabet + digit + special),choice(alphabet + digit + special),choice(alphabet + digit + special),sep="")
+
+# By Anubhav Kumar Gupta 
