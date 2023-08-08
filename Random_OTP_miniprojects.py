083381da32c36d257a565849e7dd1551e1d5fa3f
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2023-08-08 16:54:14 +0530

    Add files via upload

---
+++ Random_OTP_miniprojects.py
@@ -0,0 +1,4 @@
+from random import *
+print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep="")
+
+# This Python Script contain RANDOM OTP generator with digits only.
