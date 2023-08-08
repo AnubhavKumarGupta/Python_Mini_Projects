8e01a81206b855c86273270a160fa9e245ccabf9
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2023-08-08 16:52:12 +0530

    Add files via upload

---
+++ Random_Password_AlphaNumeric.py
@@ -0,0 +1,7 @@
+from random import *
+alphabet="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+digit="0123456789"
+
+print(choice(digit),choice(alphabet),choice(digit),choice(alphabet),choice(digit),choice(alphabet),choice(digit),choice(alphabet),sep="")
+
+# By Anubhav Kumar Gupta 
