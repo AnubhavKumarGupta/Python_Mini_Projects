a6965b432cbcbaed1faf9447601b1338810974a9
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2024-10-04 19:25:39 +0530

    Add files via upload

---
+++ bar_code_generator.py
@@ -0,0 +1,6 @@
+# this programm is used to create qr code for the given link.
+
+# import pyqrcode
+# qr = pyqrcode.create('https://youtu.be/PIyf0hMc498?si=su0KvmDJvjN6r6Bl')
+# qr.png("image.png")
+
---
+++ calender.py
@@ -0,0 +1,4 @@
+#calender using python
+
+import calendar
+print(calendar.month(2023,9))
